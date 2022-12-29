cf0f8ccb2757881297542d7ff30631b1f1a9ce42
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-12-29 11:24:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS2O-PWA-Template-HTML
+
+[![Mr Coxall's Super Linter](https://github.com/sam-corbett/ICS2O-PWA-Test/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/sam-corbett/ICS2O-PWA-Test/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/sam-corbett/ICS2O-PWA-Test)](https://repl.it/github/sam-corbett/ICS2O-PWA-Test)
+
+This site can be found at: [https://sam-corbett.github.io/ICS2O-PWA-Test/](https://sam-corbett.github.io/ICS2O-PWA-Test/)
---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
