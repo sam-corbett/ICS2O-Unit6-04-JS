d0966cfbedf6f6640b188cd6e8ebc8ba1dbe7e09
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-12-31 17:33:46 +0000

    code added in script.js

--- js/script.js
+++ js/script.js
@@ -15,9 +15,18 @@ if (navigator.serviceWorker) {
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+window.onload = function() {
+
+// this calculates the volume of the sphere
+ const params = new URLSearchParams(document.location.search)
+
+  //input
+  const radius = parseFloat(params.get("r"))
+
+  //process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+
+  //output
+  document.getElementById("radius").innerHTML = "<h6>Radius = " + radius + "</h6)"
+  document.getElementById("volume").innerHTML = "<h6>Volume is: " + volume.toFixed(2) + " cm³</h6>"
 }
